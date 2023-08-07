8e8fc347dc71fa2794e97e7d8f9bb638b267fa7d
mariacancian <mariacancian.564@gmail.com>
2023-08-07 15:02:04 -0300

    creat edit and remove posts functions"

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -14,27 +14,90 @@
 //     document.getElementById("resposta").innerHTML = 'bem vindo ' + personSimple.firstName +'seu sobrenome é '+personSimple.lastName +'sua idade é ' + personSimple.firstAge + 'sua cor é ' + personSimple.firstColor;
 // }
 
-const post= [];
-function savePOst(){
+const posts = [];
+let indexPost = -1;
+function savePost() {
 
     const title = document.getElementById("title").value;
     const resume = document.getElementById("resume").value;
     const publisher = document.getElementById("publisher").value;
     const date = document.getElementById("date").value;
- 
-    if(title && resume && publisher && date){
-        storePost(title, resume, publisher, date);
+
+    console.log(title, resume, publisher, date);
+
+
+    if (indexPost == -1) {
+        if (title && resume && publisher && date) {
+            storePost(title, resume, publisher, date);
+            cleanFields();
+        } else {
+            if (title && resume && publisher && date) {
+                posts[indexPost] = [
+                    title,
+                    resume,
+                    publisher,
+                    date
+                ];
+
+                showPosts();
+                indexPost = -1;
+                cleanFields();
+            }
+        }
     }
 }
 
-function storePost(title, resume, publisher, date){
-    const post = {
-        title,
-        resume,
-        publisher,
-        date
-    };
-    post.push(post)
-}
+    function storePost(title, resume, publisher, date) {
+        const post = {
+            title,
+            resume,
+            publisher,
+            date
+        };
 
+        posts.push(post);
+        showPosts();
+    }
+
+    function showPosts() {
+        let showContent = "";
+
+        posts.forEach((post, index) => {
+
+            showContent += `
+        <div class= "post">
+        <h2>${post.title}</h2>
+        <p><srong>Resumo:</strong>${post.resume}</p>
+        <p><srong>Autor:</strong>${post.publisher}</p>
+        <p><srong>data de punlicacão:</strong>${post.date}</p>
+
+        <button onclick ='editPost(${index})'>Editar</button>
+        <button onclick ='removePost(${index})'>Remover</button>
+        
+    </div>`;
+        })
+        document.getElementById('list').innerHTML = showContent;
+    }
+
+    function cleanFields() {
+        document.getElementById("title").value = "";
+        document.getElementById("resume").value = "";
+        document.getElementById("publisher").value = "";
+        document.getElementById("date").value = "";
+    }
+
+    function editPost(index) {
+        const post = posts[index];
+        indexPost = index;
+        document.getElementById("title").value = post.title;
+        document.getElementById("resume").value = post.title;
+        document.getElementById("publisher").value = post.title;
+        document.getElementById("date").value = post.title;
+    }
+
+    function removePost(index){
+        posts.splice(index, 1);
+
+        showPosts();
+    }
 
